58b20799e83927055300849efd6666aae960ca8d
Magnus Helmersson <magnus.helmersson@gmail.com>
2015-06-25 20:21:46 +0200

    Less mess if executed twice.
    Need alternative to lookbehind to fix the one in the parenthesis.

--- js/unitconv.js
+++ js/unitconv.js
@@ -3,7 +3,7 @@ function convertTextareas() {
 		console.log(textareas.length);
     for (var i = 0; i < textareas.length; i++) {
         textareas[i].value = textareas[i].value.replace(
-            /\d+\.?\d+\s+(kg|lbs)/g, 
+            /\d+\.?\d+\s+(kg|lbs?)(?! \(\d+\.?\d+\s+(?:kg|lbs?)\))/g,
             function (match, capture) {
                 if(capture == 'kg') {
                     factor = 2.20462;
@@ -12,7 +12,7 @@ function convertTextareas() {
                     factor = 0.453592;
                     unit = 'kg';
                 }
-                return match + ' (' + match.replace(/(\d+\.?\d+)\s+(?:kg|lbs)/g,
+                return match + ' (' + match.replace(/(\d+\.?\d+)\s+(?:kg|lbs?)/g,
                                      function (match2, capture2) {
                                          return Math.round(capture2 * factor * 10) / 10;
                                      }) + ' ' + unit + ')';
