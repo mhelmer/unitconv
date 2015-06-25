a03102040d47fcc4d17c4ca8bff2abe75859bd5c
Magnus Helmersson <magnus.helmersson@gmail.com>
2015-06-25 12:11:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+```
+javascript:(function(){s=document.createElement('script');s.type='text/javascript';s.src='https://mhelmer.github.io/unitconv/js/unitconv.js?v='+parseInt(Math.random()*99999999);document.body.appendChild(s);parseText();})();
+
+javascript:(function(){s=document.createElement('script');s.type='text/javascript';s.src='https://mhelmer.github.io/unitconv/js/unitconv.js?v='+parseInt(Math.random()*99999999);document.body.appendChild(s);parseText();})();
+```
+
+
---
+++ js/unitconv.js
@@ -0,0 +1,21 @@
+function convertTextareas() {
+    var textareas = document.getElementsByTagName("textarea");
+    for (var i = 0; i < textareas.length; i++) {
+        textareas[i].value = textareas[i].value.replace(
+            /\d+\.?\d+\s+(kg|lbs)/g, 
+            function (match, capture) {
+                if(capture == 'kg') {
+                    factor = 2.20462;
+                    unit = 'lbs';
+                } else {
+                    factor = 0.453592;
+                    unit = 'kg';
+                }
+                return match + ' (' + match.replace(/(\d+\.?\d+)\s+(?:kg|lbs)/g,
+                                     function (match2, capture2) {
+                                         return Math.round(capture2 * factor * 10) / 10;
+                                     }) + ' ' + unit + ')';
+            });
+       return false;
+    }
+};
