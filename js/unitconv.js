84a0781e9ffe8d423342b843f0887c90f128c703
Magnus Helmersson <magnus.helmersson@gmail.com>
2015-06-25 15:10:56 +0200

    fix return inside for loop

--- js/unitconv.js
+++ js/unitconv.js
@@ -1,5 +1,6 @@
 function convertTextareas() {
     var textareas = document.getElementsByTagName("textarea");
+		console.log(textareas.length);
     for (var i = 0; i < textareas.length; i++) {
         textareas[i].value = textareas[i].value.replace(
             /\d+\.?\d+\s+(kg|lbs)/g, 
@@ -16,6 +17,6 @@ function convertTextareas() {
                                          return Math.round(capture2 * factor * 10) / 10;
                                      }) + ' ' + unit + ')';
             });
-       return false;
     }
+		return false;
 };
